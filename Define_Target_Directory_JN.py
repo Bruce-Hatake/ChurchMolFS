de8e244a74b62acb1837451649e5d04afd0e1de5
Bruce-Hatake <pwjackson@aggies.ncat.edu>
2025-03-11 18:10:03 -0400

    Create Define_Target_Directory_JN.py

---
+++ Define_Target_Directory_JN.py
@@ -0,0 +1,4 @@
+# Set the target directory for Jupyter Notebook in Cell 1. Then proceed with the encoder for cell 2, the decoder for cell 3, the church interface for cell 4, the test church for cell 5 
+# and then the Run_Test_JN for Cell 6
+save_dir = ""
+print(f"Files will be saved to: {save_dir}")
